ca9037c95c25f8516f548b93c1017bcea6ff669a
Thibaud Dauce <git@hadibut.fr>
2022-10-04 12:35:36 +0200

    Fix wrong type

--- ios/Classes/CloudproofPlugin.swift
+++ ios/Classes/CloudproofPlugin.swift
@@ -1,6 +1,6 @@
 class CloudproofPlugin {
     public func dummyMethodToEnforceBundling() {
         // This will never be executed
-        h_get_encrypted_header_size(nil, nil);
+        h_get_encrypted_header_size(nil, 0);
     }
 }
