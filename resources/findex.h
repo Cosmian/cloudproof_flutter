83a151140b532b4f7a9918a1b4e36471d920757c
Manuthor <manu.coste@gmail.com>
2023-01-13 22:18:32 +0100

    fix: remove native libs and download them via script

--- resources/findex.h
+++ resources/findex.h
@@ -196,7 +196,7 @@ typedef int (*ListRemovedLocationsCallback)(unsigned char *removed_locations_ptr
  * # Parameters
  *
  * - `indexed_values`          : (output) search result
- * - `master_key`              : masterkey
+ * - `master_key`              : master key
  * - `label`                   : additional information used to derive Entry
  *   Table UIDs
  * - `keywords`                : `serde` serialized list of base64 keywords
