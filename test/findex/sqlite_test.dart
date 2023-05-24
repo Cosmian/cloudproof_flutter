9e55a5b3496910e1354391b417ef0864c604e1ae
Manuthor <manu.coste@gmail.com>
2023-05-24 14:26:39 +0200

    fix: Tests on published package -> Missing generated ffi bindings in published package

--- test/findex/sqlite_test.dart
+++ test/findex/sqlite_test.dart
@@ -140,41 +140,6 @@ void main() {
       }
     });
 
-    test('search/upsert', () async {
-      const dbPath = "./build/sqlite.db";
-
-      await initDb(dbPath);
-
-      final masterKey = FindexMasterKey.fromJson(jsonDecode(
-          await File('test/resources/findex/master_key.json').readAsString()));
-
-      final label = Uint8List.fromList(utf8.encode("Some Label"));
-
-      SqliteFindex.init(dbPath);
-      expect(SqliteFindex.count('entry_table'), equals(0));
-      expect(SqliteFindex.count('chain_table'), equals(0));
-
-      await SqliteFindex.indexAll(masterKey, label);
-
-      expect(SqliteFindex.count('entry_table'), equals(583));
-      expect(SqliteFindex.count('chain_table'), equals(618));
-
-      final searchResults = await SqliteFindex.search(
-          masterKey.k, label, [Keyword.fromString("France")]);
-
-      expect(searchResults.length, 1);
-
-      final keyword = searchResults.entries.toList()[0].key;
-      final indexedValues = searchResults.entries.toList()[0].value;
-      final usersIds = indexedValues.map((indexedValue) {
-        return indexedValue.location.bytes[0];
-      }).toList();
-      usersIds.sort();
-
-      expect(Keyword.fromString("France").toBase64(), keyword.toBase64());
-      expect(usersIds, equals(expectedUsersIdsForFrance));
-    });
-
     Future<void> insertNewIndexes(
         String dbPath,
         FindexMasterKey masterKey,
@@ -186,12 +151,6 @@ void main() {
 
       expect(SqliteFindex.count('entry_table'), equals(0));
       expect(SqliteFindex.count('chain_table'), equals(0));
-
-      // final indexedValuesAndKeywords = {
-      //   IndexedValue.fromLocation(Location(Uint8List.fromList([id]))): [
-      //     Keyword.fromString(word)
-      //   ]
-      // };
 
       await SqliteFindex.upsert(masterKey, label, indexedValuesAndKeywords);
 
@@ -333,6 +292,41 @@ void main() {
           expect(usersIds, equals([1, 2]));
         }
       }
+    });
+
+    test('search/upsert', () async {
+      const dbPath = "./build/sqlite.db";
+
+      await initDb(dbPath);
+
+      final masterKey = FindexMasterKey.fromJson(jsonDecode(
+          await File('test/resources/findex/master_key.json').readAsString()));
+
+      final label = Uint8List.fromList(utf8.encode("Some Label"));
+
+      SqliteFindex.init(dbPath);
+      expect(SqliteFindex.count('entry_table'), equals(0));
+      expect(SqliteFindex.count('chain_table'), equals(0));
+
+      await SqliteFindex.indexAll(masterKey, label);
+
+      expect(SqliteFindex.count('entry_table'), equals(583));
+      expect(SqliteFindex.count('chain_table'), equals(618));
+
+      final searchResults = await SqliteFindex.search(
+          masterKey.k, label, [Keyword.fromString("France")]);
+
+      expect(searchResults.length, 1);
+
+      final keyword = searchResults.entries.toList()[0].key;
+      final indexedValues = searchResults.entries.toList()[0].value;
+      final usersIds = indexedValues.map((indexedValue) {
+        return indexedValue.location.bytes[0];
+      }).toList();
+      usersIds.sort();
+
+      expect(Keyword.fromString("France").toBase64(), keyword.toBase64());
+      expect(usersIds, equals(expectedUsersIdsForFrance));
     });
 
     test('nonRegressionTest', () async {
