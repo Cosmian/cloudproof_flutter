3b36e02b4ef38e979109f19b0bf393f2e1cc111c
Manuthor <manu.coste@gmail.com>
2023-06-02 15:38:32 +0200

    chore: next release 7.0.0

--- scripts/get_native_libraries.py
+++ scripts/get_native_libraries.py
@@ -94,6 +94,6 @@ def download_native_libraries(version: str) -> bool:
 
 
 if __name__ == '__main__':
-    ret = download_native_libraries('v2.0.0')
+    ret = download_native_libraries('v2.0.1')
     if ret is False and os.getenv('GITHUB_ACTIONS'):
-        download_native_libraries('last_build/update_findex_multi_fetch_entry_tables')
+        download_native_libraries('last_build/release/v2.0.1')
