77a327808e4bac23614714649e21d4e721089c14
Manuthor <manu.coste@gmail.com>
2023-06-14 10:19:45 +0200

    fix: missing dep for example project

--- example/integration_test/app_test.dart
+++ example/integration_test/app_test.dart
@@ -6,7 +6,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
 import 'package:cloudproof/cloudproof.dart';
-import 'package:cloudproof_demo/main.dart' as app;
+// import 'package:cloudproof_demo/main.dart' as app;
 
 import '../../test/findex/sqlite_findex.dart';
 
@@ -16,7 +16,7 @@ void main() {
   group('end-to-end test', () {
     testWidgets('tap on the floating action button, verify counter',
         (tester) async {
-      app.main();
+      // app.main();
 
       final encrypted = base64Decode(
           "3DnOR7troSUW3TJA7rRltGzkqz5eVzfqOnTLupvliy/w7XRzQ6uPIkiRjtfHnOwH7ewmUHbiq/8Di9/RZQuWIAEtfSF6I+gHUChts2uoQklvQ4miSoLx8KYacB2VxYfOu7ad8DdoWK8uShldAhP6vl0tgmSvWW+Qn5q7OoIjjU4PIgoeFPl1REJ93rPizbde2nM/wkHEqDvcbVvOScvApjdn7hfmLIWGRghzZLBJj+4wFBoAAMdzZGuiZ9QnG0dXmcZdjgKH5ZpHTDjbdR4JraI7FaF8");
