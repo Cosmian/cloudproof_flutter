599fbfc3222bb00b3330177188312b097e3cee51
Manuthor <manu.coste@gmail.com>
2023-11-27 08:43:30 +0100

    fix: get last release of cp_rust

--- scripts/get_native_libraries.py
+++ scripts/get_native_libraries.py
@@ -98,5 +98,5 @@ def download_native_libraries(version: str) -> bool:
 if __name__ == '__main__':
     RET = download_native_libraries('v2.4.0')
     if RET is False and os.getenv('GITHUB_ACTIONS'):
-        download_native_libraries('last_build/feature/ffi_instance')
+        download_native_libraries('last_build/fix/ffi_unwrap_return_code')
     write_ios_cloudproof_plugin_header()
