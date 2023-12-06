7051751e2e6f51d32abbfc6f00b7acb5cc446984
Manuthor <manu.coste@gmail.com>
2023-12-06 15:06:27 +0100

    fix: integration test

--- example/integration_test/app_test.dart
+++ example/integration_test/app_test.dart
@@ -30,8 +30,7 @@ void main() {
       expect(result.plaintext, equals(plaintext));
 
       final findexKey = FindexKey(base64Decode("6hb1TznoNQFvCWisGWajkA=="));
-      final label = Uint8List.fromList(utf8.encode("Some Label"));
-      await testFunction(findexKey, label);
+      await testFunction(findexKey, "Some Label");
 
       print("findex and cover_crypt OK");
     });
